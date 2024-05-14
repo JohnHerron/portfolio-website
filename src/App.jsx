f61136da107dd5f804c6fa8ac08240bdcee6b2ec
John Herron <herroncjohn@gmail.com>
2024-05-14 01:54:43 -0700

    ADD: initial project react files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import './App.css';
+
+function App() {
+
+  return (
+    <>
+      <div>
+        <h1>JOHN HERRON</h1>
+        <p>Hello World!</p>
+      </div>
+    </>
+  );
+}
+
+export default App;
