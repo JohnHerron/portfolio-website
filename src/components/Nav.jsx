ffb320cef6ac12f96117e6730b9a47d483255d12
John Herron <herroncjohn@gmail.com>
2024-05-20 14:04:50 -0700

    ADD: navigation link is clickable when button is highlighted, instead of only the text

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -9,27 +9,27 @@ export default function Nav() {
                 <h1 className={style.name}>John Herron</h1>
             </div>
             <div className={style.btnContainer}>
-                <button className={style.button}>
-                    <Link className={style.btnLink} to='contact' offset={-250} smooth={true} duration-500>
+                <Link className={style.btnLink} to='contact' offset={-250} smooth={true} duration-500>
+                    <button className={style.button}>
                         Contact
-                    </Link>
-                </button>
-                <button className={style.button}>
-                    <Link className={style.btnLink} to='projects' offset={-250} smooth={true} duration-500>
+                    </button>
+                </Link>
+                <Link className={style.btnLink} to='projects' offset={-250} smooth={true} duration-500>
+                    <button className={style.button}>
                         Projects
-                    </Link></button>
-                <button className={style.button}>
-                    <Link className={style.btnLink} to='experience' offset={-250} smooth={true} duration-500>
+                    </button>
+                </Link>
+                <Link className={style.btnLink} to='experience' offset={-250} smooth={true} duration-500>
+                    <button className={style.button}>
                         Experience
-                    </Link>
-                </button>
-                <button className={style.button}>
-                    <Link className={style.btnLink} to='about' offset={-400} smooth={true} duration-500>
+                    </button>
+                </Link>
+                <Link className={style.btnLink} to='about' offset={-400} smooth={true} duration-500>
+                    <button className={style.button}>
                         About
-                    </Link>
-                </button>
+                    </button>
+                </Link>
             </div>
-            
         </div>
     );
 }
